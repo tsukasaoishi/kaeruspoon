fd4e8af737d065999fee959a5a8507de74964195
tsukasaoishi <tsukasa.oishi@gmail.com>
2013-01-26 09:00:39 +0900

    Make App by edge Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaeruspoon::Application.config.secret_key_base = 'bbde715a24cfa3ef76307078a1f162744520cf468c1d4a219398170bfdf661ab4f1082cf4b031137ecc1a793bc74a540a09bd04757c127dca4464ece3463d087'
