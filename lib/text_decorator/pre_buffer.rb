e5abdf8552206191b1e1e2e949327a8271ac1600
tsukasaoishi <tsukasa.oishi@gmail.com>
2013-03-13 23:06:38 +0900

    other language support

--- lib/text_decorator/pre_buffer.rb
+++ lib/text_decorator/pre_buffer.rb
@@ -3,11 +3,13 @@ class TextDecorator
   # build <pre> tag
   #
   class PreBuffer < Array
-    REGEXP_PRE_BEGIN = /^>\|\|$/
+    REGEXP_PRE_BEGIN = /^>\|(.*)\|$/
     REGEXP_PRE_END = /^\|\|<$/
 
     def begin?(str)
-      @on = (str =~ REGEXP_PRE_BEGIN)
+      @on = (str =~ REGEXP_PRE_BEGIN).tap do
+        @lang = $1.presence
+      end
     end
 
     def finish?(str)
@@ -21,7 +23,9 @@ class TextDecorator
     def flash
       return "" if empty?
 
-      CodeRay.scan(join("\n"), :ruby).div
+      lang = @lang
+      @lang = nil
+      CodeRay.scan(join("\n"), lang || :ruby).div
     ensure
       @on = false
       clear
