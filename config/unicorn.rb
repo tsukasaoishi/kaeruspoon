7caef7655142459903aed1f69a32ff7d588ed892
tsukasaoishi <tsukasa.oishi@gmail.com>
2013-12-14 11:27:13 +0900

    config of unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,29 +2,31 @@ worker_processes 3
 working_directory '/home/rails/kaeru'
 listen 9292, :tcp_nopush => true
 timeout 30
-pid 'tmp/pids/puma.pid'
+pid 'tmp/pids/unicorn.pid'
 preload_app true
 stderr_path 'log/unicorn.log'
 stdout_path 'log/unicorn.log'
 
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
   defined?(ActiveRecord::Base) and
   ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+
+  sleep 1
 end
 
 after_fork do |server, worker|
-
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-  end
-
   defined?(ActiveRecord::Base) and
   ActiveRecord::Base.establish_connection
+
   Rails.cache.instance_variable_get(:@data).reset if Rails.cache.instance_variable_get(:@data).respond_to?(:reset)
 end
