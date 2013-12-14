e26833bc245afd9f719aef50ab8197226c8a3b15
tsukasaoishi <tsukasa.oishi@gmail.com>
2013-12-14 11:17:50 +0900

    keyword crud

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,7 +1,42 @@
 class KeywordsController < ApplicationController
+  before_filter :required_login, only: [:new, :create, :edit, :update, :destroy]
+
   def show
-    raise ActiveRecord::RecordNotFound unless @keyword = Keyword.where(:name => params[:id]).first
+    @keyword = Keyword.where(:name => params[:id]).first ||
+      Keyword.new(:name => params[:id])
     @title = @keyword.name
     @articles = @keyword.articles.paginate(:order => "articles.publish_at DESC", :per_page => 20, :page => params[:page])
+  end
+
+  def new
+    @keyword = Keyword.new
+  end
+
+  def create
+    keyword = Keyword.create!(require_params)
+    redirect_to keyword_path(:id => keyword.name)
+  end
+
+  def edit
+    @keyword = Keyword.find(params[:id])
+    render "new"
+  end
+
+  def update
+    keyword = Keyword.find(params[:id])
+    keyword.update_attributes!(require_params)
+    redirect_to keyword_path(:id => keyword.name)
+  end
+
+  def destroy
+    keyword = Keyword.find(params[:id])
+    keyword.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def require_params
+    params.require(:keyword).permit(:name, :body)
   end
 end
