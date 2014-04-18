07e137b57775cab653cd61f7335baf61fef5998a
tsukasaoishi <tsukasa.oishi@gmail.com>
2014-04-18 23:20:12 +0900

    separated counter logic

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  include Counter
+
   has_one :content, :class_name => "ArticleContent", :dependent => :destroy
   has_many :article_keywords, :dependent => :destroy
   has_many :keywords, :through => :article_keywords
@@ -41,13 +43,17 @@ class Article < ActiveRecord::Base
   end
 
   def prev_article(user = nil)
-    articles = user ? user.articles : self.class.default_scoped
-    @prev_article ||= articles.where("publish_at <= ? AND id <> ?", publish_at, id).newest.first
+    @prev_article ||= begin
+      articles = user ? user.articles : self.class.default_scoped
+      articles.where("publish_at <= ? AND id <> ?", publish_at, id).newest.first
+    end
   end
 
   def next_article(user = nil)
-    articles = user ? user.articles : self.class.default_scoped
-    @next_article ||= articles.where("publish_at >= ? AND id <> ?", publish_at, id).oldest.first
+    @next_article ||= begin
+      articles = user ? user.articles : self.class.default_scoped
+      articles.where("publish_at >= ? AND id <> ?", publish_at, id).oldest.first
+    end
   end
 
   def body
@@ -62,24 +68,6 @@ class Article < ActiveRecord::Base
     @_pickup_photo ||= begin
       photo_id = plain_body.scan(/\[p\:(\d+)\]/).flatten.first
       photo_id && Photo.find_by_id(photo_id)
-    end
-  end
-
-  def count_up
-    if buffer = Rails.cache.read(counter_key)
-      count, set_time = buffer.split(":").map{|i| i.to_i}
-      count += 1
-      if (Time.now.to_i - set_time) > 600
-        Rails.cache.delete(counter_key)
-        self.access_count += count
-        self.save
-      else
-        write_counter_cache(count, set_time)
-      end
-    else
-      self.access_count += 1
-      self.save
-      write_counter_cache(0, Time.now.to_i)
     end
   end
 
@@ -109,10 +97,6 @@ class Article < ActiveRecord::Base
 
   def plain_body
     @_plain_body ||= body.gsub(/[\r\n]/, "").gsub(/>\|.*?\|.+?\|\|</, "")
-  end
-
-  def write_counter_cache(count, set_time)
-    Rails.cache.write(counter_key, "#{count}:#{set_time}", expires_in: 4.weeks)
   end
 
   def counter_key
