378e8daa224caaad288e70b7afebf79a2d0d7cd4
tsukasaoishi <tsukasa.oishi@gmail.com>
2014-04-20 01:09:43 +0900

    expire cache when destroy article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -64,6 +64,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     article = Article.find(params[:id])
+    expire_action(article)
     article.destroy
     redirect_to root_path
   end
