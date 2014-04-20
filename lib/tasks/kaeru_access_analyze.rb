56363992946b7936a9a98cae972d8bc6df600acc
tsukasaoishi <tsukasa.oishi@gmail.com>
2014-04-20 15:27:41 +0900

    delete unnecessaliry condition

--- lib/tasks/kaeru_access_analyze.rb
+++ lib/tasks/kaeru_access_analyze.rb
@@ -6,7 +6,7 @@ module Tasks
       if line =~ %r!\[([^\]]+)\]\s+"GET\s+/articles/(\d+)\s+HTTP/\d\.\d"\s+(\d+)\s!
         work = $1.split(":", 2)
         time = Time.parse(work.join(" ")) rescue nil
-        return if !time || (checked_time && time <= checked_time)
+        return if !time || time <= checked_time
         article_id, code = [$2, $3].map{|item| item.to_i}
         return if article_id == 0
         return unless (200...400).include?(code)
