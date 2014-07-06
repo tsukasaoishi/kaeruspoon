3e0c87ffb170625cf1caa1ab58c0193dd834d42e
tsukasaoishi <tsukasa.oishi@gmail.com>
2014-07-06 22:08:54 +0900

    add markdown when photo upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
     article = Article.find_by_id(params[:article_id])
     photo = Photo.create!(params[:photo].permit!)
     session[:backup_article_title] = params[:backup_article_title]
-    session[:backup_article_body] = params[:backup_article_body] + "[p:#{photo.id}]\n"
+    session[:backup_article_body] = params[:backup_article_body] + %Q|![Large](#{photo.image.url(:large)})\n|
 
     if article
       redirect_to edit_article_path(article)
