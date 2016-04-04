5eff7be1324c7294db10406db66af479d34d5a0a
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2016-04-05 08:39:22 +0900

    Changed popular articles size

--- app/controllers/articles/popular_controller.rb
+++ app/controllers/articles/popular_controller.rb
@@ -2,7 +2,7 @@ class Articles::PopularController < ApplicationController
   caches_action :index, expires_in: 1.week
 
   def index
-    @articles = Article.popular_articles(20)
+    @articles = Article.popular_articles(50)
     @title = I18n.t(:popular_articles)
   end
 end
