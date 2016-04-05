44c3728adef45bc4127c309a8a25c7811bbb8801
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2016-04-06 02:00:32 +0900

    Edited paginate design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  MARKS = {
+    previous: "&#8592; ".html_safe,
+    next: " &#8594;".html_safe
+  }
+  def previous_link_to(name, url, options = {})
+    name = MARKS[:previous] + h(name)
+    link_to(name, url, options)
+  end
+
+  def next_link_to(name, url, options = {})
+    name = h(name) + MARKS[:next]
+    link_to(name, url, options)
+  end
 end
